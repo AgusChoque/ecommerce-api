5bcef411cc2dd519367c34f48185c244a176b90b
agustinchoque <agustinchoque.dev@gmail.com>
2025-02-18 22:50:54 -0300

    Homework 5: TypeOrm Config

--- ecommerce-AgusChoque/src/app.module.ts
+++ ecommerce-AgusChoque/src/app.module.ts
@@ -2,9 +2,28 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './modules/products/products.module';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductsModule, UsersModule, AuthModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: ".env.development"
+  }), 
+  TypeOrmModule.forRootAsync({
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => ({
+      type: "postgres",
+      database: configService.get("DB_NAME"),
+      host: configService.get("DB_HOST"),
+      port: configService.get("DB_PORT"),
+      username: configService.get("DB_USERNAME"),
+      password: configService.get("DB_PASSWORD"),
+      entities: [],
+      synchronize: true,
+    })
+  })
+    , ProductsModule, UsersModule, AuthModule],
   controllers: [],
   providers: [],
 })
